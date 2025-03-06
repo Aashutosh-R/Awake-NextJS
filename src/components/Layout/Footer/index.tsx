019a21bc0c2fb6600725544c714da103e3ad6e57
ashutosh <radaashutosh556@gmail.com>
2025-03-06 18:38:13 +0530

    footer dark mode setup

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
   return (
     <footer>
       <div className='container mx-auto max-w-[1272px] pt-20 pb-6 px-7'>
-        <div className='mb-16 py-16 border-b border-black/10'>
+        <div className='mb-16 py-16 border-b border-black/10 dark:border-white/10'>
           <div className='grid grid-cols-6 sm:grid-cols-12 md:gap-16 gap-12'>
             <div className='flex flex-col gap-6 md:col-span-5 sm:col-span-6 col-span-5'>
               <div>
@@ -16,7 +16,7 @@ const Footer: React.FC = () => {
                 </Link>
               </div>
               <div>
-                <p className='text-base font-normal text-black/60 leading-5'>
+                <p className='text-base font-normal text-black/60 dark:text-white/60 leading-5'>
                   Empowering businesses with innovative solutions. <br />{' '}
                   Let&apos;s create something amazing together.
                 </p>
@@ -28,7 +28,7 @@ const Footer: React.FC = () => {
                       icon={item.icon}
                       width={20}
                       height={20}
-                      className='text-black/60 hover:text-black'
+                      className='text-black/60 dark:text-white/60 hover:text-black dark:hover:text-white'
                     />
                   </Link>
                 ))}
@@ -43,7 +43,7 @@ const Footer: React.FC = () => {
                   {item.links?.map((linkitem, linkindex) => (
                     <li
                       key={linkindex}
-                      className='text-base font-normal text-black/60 hover:text-black'>
+                      className='text-base font-normal text-black/60 dark:text-white/60 hover:text-black dark:hover:text-white'>
                       <Link href={linkitem.href}>{linkitem.link}</Link>
                     </li>
                   ))}
@@ -52,7 +52,7 @@ const Footer: React.FC = () => {
             ))}
             <div className='md:col-span-3 xxs:col-span-6 col-span-5'>
               <p className='text-base font-medium mb-4'>Contact Details</p>
-              <div className='text-base font-normal text-black/60 flex flex-col gap-3'>
+              <div className='text-base font-normal flex flex-col gap-3'>
                 <p>
                   81 Rivington Street London <br />
                   EC2A 3AY
@@ -64,7 +64,7 @@ const Footer: React.FC = () => {
           </div>
         </div>
         <div>
-          <p className='text-sm font-normal text-black/40 text-center'>
+          <p className='text-sm font-normal text-black/40 dark:text-white/40 text-center'>
             ©2025 Awake. All Rights Reserved
           </p>
         </div>
