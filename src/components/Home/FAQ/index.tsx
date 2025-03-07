e42a939f35b45be2ba6d591bee3da1956b0b64e7
ashutosh <radaashutosh556@gmail.com>
2025-03-07 15:04:59 +0530

    faq section accordian updated (open on click anywhere in accordian)

--- src/components/Home/FAQ/index.tsx
+++ src/components/Home/FAQ/index.tsx
@@ -14,46 +14,55 @@ const FAQ: React.FC = () => {
     <section>
       <div className='container mx-auto max-w-[1272px] py-20 px-7'>
         <div className='mb-20'>
-          <h3 className='text-5xl font-medium text-center leading-14'>
+          <h3 className='text-4xl lg:text-5xl font-medium text-center leading-14'>
             Got questions? <br />
             We’ve got{' '}
             <span className='InstrumentS italic font-normal'>answers</span>
           </h3>
         </div>
         <div>
-          <ul className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-4'>
             {faq.map((item, index) => (
-              <li
+              <button
                 key={index}
-                className={`${
-                  expandedIndex === index
-                    ? 'bg-black/5 dark:bg-white/5'
-                    : 'bg-transparent'
-                } border border-black/10 dark:border-white/20 rounded-2xl px-6 py-8`}>
-                <span className='flex items-center justify-between'>
-                  <h6 className='text-xl font-medium'>{item.que}</h6>
-                  <button
-                    onClick={() => handleClick(index)}
-                    className={`transition ${
-                      expandedIndex === index ? 'rotate-45' : 'rotate-0'
-                    }`}>
-                    <Icon icon={'ph:plus'} width={20} height={20} />
-                  </button>
-                </span>
-                <p
-                  className={`overflow-hidden transition-all duration-300 ease-in-out ${
+                onClick={() => handleClick(index)}
+                className='hover:cursor-pointer'>
+                <div
+                  className={`${
                     expandedIndex === index
-                      ? 'max-h-96 opacity-100 mt-3'
-                      : 'max-h-0 opacity-0'
-                  } text-base font-normal text-black/60 dark:text-white/60`}
-                  style={{
-                    transitionProperty: 'max-height, opacity, margin-top',
-                  }}>
-                  {item.ans}
-                </p>
-              </li>
+                      ? 'bg-black/5 dark:bg-white/5'
+                      : 'bg-transparent'
+                  } border border-black/10 dark:border-white/20 rounded-2xl px-6 py-8`}>
+                  <div className='flex items-center justify-between'>
+                    <h6 className='text-xl text-left font-medium'>
+                      {item.que}
+                    </h6>
+                    <span>
+                      <Icon
+                        icon={'ph:plus'}
+                        width={20}
+                        height={20}
+                        className={`transition ${
+                          expandedIndex === index ? 'rotate-45' : 'rotate-0'
+                        }`}
+                      />
+                    </span>
+                  </div>
+                  <p
+                    className={`overflow-hidden transition-all duration-300 ease-in-out ${
+                      expandedIndex === index
+                        ? 'max-h-96 opacity-100 mt-3'
+                        : 'max-h-0 opacity-0'
+                    } text-base text-left font-normal text-black/60 dark:text-white/60`}
+                    style={{
+                      transitionProperty: 'max-height, opacity, margin-top',
+                    }}>
+                    {item.ans}
+                  </p>
+                </div>
+              </button>
             ))}
-          </ul>
+          </div>
         </div>
       </div>
     </section>
