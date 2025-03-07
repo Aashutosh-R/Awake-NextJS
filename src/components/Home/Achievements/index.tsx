74bea1b1a16ce5fee9e4c6a7605571525ad64a04
ashutosh <radaashutosh556@gmail.com>
2025-03-07 15:09:38 +0530

    achievements section hover effect add

--- src/components/Home/Achievements/index.tsx
+++ src/components/Home/Achievements/index.tsx
@@ -6,7 +6,7 @@ const Achievements: React.FC = () => {
     <section>
       <div className='container mx-auto max-w-[1272px] py-20 px-7'>
         <div className='mb-20'>
-          <h3 className='text-5xl font-medium leading-14 text-center'>
+          <h3 className='text-4xl lg:text-5xl font-medium lg:leading-14 text-center'>
             Accolades and{' '}
             <span className='InstrumentS italic font-normal'>achievements</span>{' '}
             <br />
@@ -18,7 +18,7 @@ const Achievements: React.FC = () => {
             {achievement.map((item, index) => (
               <div
                 key={index}
-                className='border border-black/10 dark:border-white/10 rounded-2xl p-10 flex flex-col gap-16 grow'>
+                className='border border-black/10 dark:border-white/10 rounded-2xl p-10 flex flex-col gap-16 grow hover:cursor-pointer group'>
                 <div>
                   <Image
                     src={item.src}
@@ -39,7 +39,7 @@ const Achievements: React.FC = () => {
                   <p className='text-base font-normal text-black/60 dark:text-white/60 mb-3'>
                     {item.title}
                   </p>
-                  <h5 className='text-2xl font-medium leading-7'>
+                  <h5 className='text-2xl font-medium leading-7 group-hover:text-primary'>
                     {item.desc}
                   </h5>
                 </div>
